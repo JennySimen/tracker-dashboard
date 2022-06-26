c9fcf28bee4f12c15c89089fd01db5aa0eda8344
JennySimen <yakamjennifer@gmail.com>
2022-06-26 19:14:04 +0100

    changes on the dashboard

---
+++ js/generate_dashboard_table.js
@@ -0,0 +1,25 @@
+const generate_table = (data) => {
+    let tr = "";
+    data.forEach(user => {
+        tr += "<tr>";
+        tr += "<td>" + user["NID"] + "</td>";
+        tr += "<td>" + user["name"] + "</td>";
+        tr += "<td>" + user["gender"] + "</td>";
+        tr += "<td class=''>See more</td>";
+        tr += "</tr>";
+    });
+
+    return tr;
+}
+
+const generate_top_users = (data) => {
+    let li = "";
+    data.forEach(user => {
+        li += "<li>";
+        li += "<span class='product'>" + user["name"] + "</span>";
+        li += "<span class='price'>" + user["count"] + "</span>";
+        li += "</li>";
+    });
+
+    return li;
+}
---
+++ js/utils.js
@@ -0,0 +1,4 @@
+// adds 0 infront of number when it is less than 10
+const padO = (number) => {
+    return ('0' + number).slice(-2);
+}
