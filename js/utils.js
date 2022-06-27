a399624985c7a07bd136afbe09945aaab325b099
JennySimen <yakamjennifer@gmail.com>
2022-06-28 00:46:52 +0100

    complete generate barcode

--- js/utils.js
+++ js/utils.js
@@ -2,3 +2,12 @@
 const padO = (number) => {
     return ('0' + number).slice(-2);
 }
+
+const generate_unique_code = (val) => {
+    let codes = [];
+
+    Array(val).fill().map((i) => {
+        codes.push(Math.floor(Date.now() * Math.random()));
+    });
+    return codes;
+}
