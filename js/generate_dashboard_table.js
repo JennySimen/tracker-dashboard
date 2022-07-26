259c9dfd57042f5cb3454ec9fd0815c737f9a933
JennySimen <yakamjennifer@gmail.com>
2022-07-26 13:24:00 +0100

    js functions

--- js/generate_dashboard_table.js
+++ js/generate_dashboard_table.js
@@ -26,13 +26,39 @@ const generate_top_users = (data) => {
 
 const generate_barcode = (data) => {
     let tr = "";
-    data.forEach(barcode => {
-        tr += "<tr>";
-        tr += "<td>" + barcode["code"] + "</td>";
-        tr += "<td>" + barcode["checked_in"] + "</td>";
-        tr += "<td>" + barcode["checked_out"] + "</td>";
-        tr += "</tr>";
-    });
+    if (data.length === 0) {
+        tr += "<tr><td colspan='3'  class='no-data-text'>There are no barcodes yet!</td></tr>";
+    } else {
+        data.forEach(barcode => {
+            tr += "<tr>";
+            tr += "<td>" + barcode["code"] + "</td>";
+            tr += "<td>" + barcode["checked_in"] + "</td>";
+            tr += "<td>" + barcode["checked_out"] + "</td>";
+            tr += "</tr>";
+        });
+    }
+    return tr;
+}
 
+const generate_employee_table = (data) => {
+    let tr = "";
+
+    if (data.length === 0) {
+        tr += "<tr><td colspan='6'  class='no-data-text'>There are no employees yet!</td></tr>";
+    } else {
+        data.forEach((employee, index) => {
+            tr += "<tr>";
+            tr += "<td>" + (index + 1) + "</td>";
+            tr += "<td>" + employee["NID"] + "</td>";
+            tr += "<td>" + employee["name"] + "</td>";
+            tr += "<td>" + employee["type"] + "</td>";
+            tr += "<td>" + employee["gender"] + "</td>";
+            tr += "<td class='text-right'>" +
+                "<i class='bx bx-edit green edit-btn' data-nid=" + employee["id"] + "></i>" +
+                "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+                "<i class='bx bx-trash red del-btn' data-nid=" + employee["id"] + "></i></td>";
+            tr += "</tr>";
+        });
+    }
     return tr;
 }
