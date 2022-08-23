bea0e9c1c60a8e0622068698a43bc2d7cde227a0
JennySimen <yakamjennifer@gmail.com>
2022-08-23 16:51:34 +0100

    added the qr scanner

--- js/date.js
+++ js/date.js
@@ -7,3 +7,49 @@ const getTodayDate = () => {
     today = dd + '/' + mm + '/' + yyyy;
     return today;
 }
+
+
+const getThisWeekDate = () => {
+    const dateArr = [];
+    const noOfDays = Array(7).keys();
+    Array.from(noOfDays).map((idx) => {
+        const d = new Date(); 
+        d.setDate(d.getDate() - d.getDay() + idx);
+        dateArr.push(formatDate(d)); 
+    });
+
+    return dateArr;
+}
+
+const getThisMonthDate = () => {
+    const dateArr = [];
+    const noOfDays = Array(30).keys();
+    Array.from(noOfDays).map((idx) => {
+        const d = new Date(); 
+        d.setDate(d.getDate() - d.getDay() + idx);
+        dateArr.push(formatDate(d)); 
+    });
+
+    return dateArr;
+}
+
+const getSixMonthDate = () => {
+    const sixMonthArr = [];
+    const noOfDays = Array(182).keys();
+    Array.from(noOfDdays).map((idx) => {
+        const d = new Date();
+        d.setDate(d.getDate() - d.getDay() + idx);
+        dateArr.push(formatDate(d));
+    });
+
+    return dateArr;
+}
+
+const formatDate = (dateData) => {
+    var dd = String(dateData.getDate()).padStart(2, '0');
+    var mm = String(dateData.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = dateData.getFullYear();
+
+    dateData = dd + '/' + mm + '/' + yyyy;
+    return dateData;
+}
--- js/firebase.js
+++ js/firebase.js
@@ -3,3 +3,12 @@ const EMPLOYEE_TABLE = "employee";
 const FBB_DAILY_TABLE = "fbb_daily";
 const BARCODE = "barcode";
 const USER_FBB_COUNT = "user_fbb_count";
+
+const config = {
+    apiKey: "AIzaSyCwxMVF2fcArl-qCk0eu3VXndHRgFVn9Tw",
+    authDomain: "trackme-237.firebaseapp.com",
+    projectId: "trackme-237",
+    storageBucket: "trackme-237.appspot.com",
+    messagingSenderId: "826451219070",
+    appId: "1:826451219070:web:b3741cb1702a8cdc2dbe2f"
+}
