d27451e76c91efaab2046ea8dd9716a373953b28
JennySimen <yakamjennifer@gmail.com>
2023-08-16 23:38:02 +0100

    added new db

--- js/firebase.js
+++ js/firebase.js
@@ -3,6 +3,7 @@ const EMPLOYEE_TABLE = "employee";
 const FBB_DAILY_TABLE = "fbb_daily";
 const BARCODE = "barcode";
 const USER_FBB_COUNT = "user_fbb_count";
+const COUNTER = "counter";
 
 const WORKERS = "WORKER"
 const SECURITY = "SECURITY"
